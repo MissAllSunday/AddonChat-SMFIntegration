be081c20c4126837f02baa1338f1f6a714ca0f51
Suki <missallsunday@simplemachines.org>
2012-10-15 19:15:36 -0500

    Safety first!
    Signed-off-by: Suki <missallsunday@simplemachines.org>

--- Sources/AddonChat/AddonChatServer.php
+++ Sources/AddonChat/AddonChatServer.php
@@ -123,6 +123,7 @@ class AddonChatServer extends Addonchat
 
 			/* Load the users info */
 			$ids = loadMemberData($usernames, true, 'normal');
+			$user = array();
 
 			if (!empty($ids) && is_array($ids))
 				foreach ($ids as $i)
